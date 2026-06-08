3f0308990e00ad153124f3fd07401b9e0a897f8f
vufimcev <buktor@bk.ru>
2026-06-07 20:49:26 -0700

    chore: add Redux, React Hook Form, Zod, and Vitest testing setup

---
+++ src/App.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+describe('App', () => {
+  it('renders project title', () => {
+    render(<App />);
+
+    expect(
+      screen.getByRole('heading', { level: 1, name: 'React Forms' }),
+    ).toBeInTheDocument();
+  });
+});
